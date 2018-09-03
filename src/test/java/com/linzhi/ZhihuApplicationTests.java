03e10c3fcfdf2412015494b994641182b7979345
linzhi <739769483@qq.com>
2018-09-03 17:42:52 +0800

    1) 将私信未读的数量处理了一下， 如超过99条未读就写成99+。 2) 修复主页时间显示问题

--- src/test/java/com/linzhi/ZhihuApplicationTests.java
+++ src/test/java/com/linzhi/ZhihuApplicationTests.java
@@ -1,16 +1,34 @@
 package com.linzhi;
 
+import com.linzhi.dao.MessageDAO;
+import com.linzhi.model.Message;
+import com.linzhi.service.MessageService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.Date;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ZhihuApplication.class)
 public class ZhihuApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    MessageDAO messageDAO;
+
+    @Test
+    public void testAddMessage() {
+        for (int i = 0; i < 105; i++) {
+            Message message = new Message();
+            message.setContent("你好测试" + i);
+            message.setCreatedDate(new Date());
+            message.setFromId(16);
+            message.setToId(15);
+            message.setHasRead(0);
+            messageDAO.addMessage(message);
+        }
+    }
 
 }
